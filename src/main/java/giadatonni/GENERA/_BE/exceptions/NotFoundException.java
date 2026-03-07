ea63eeb27c5fca116bc1f0fc280842be77763995
Giada <tonni.giada.210802@gmail.com>
2026-03-07 10:15:56 +0100

    adds exceptions

---
+++ src/main/java/giadatonni/GENERA/_BE/exceptions/NotFoundException.java
@@ -0,0 +1,13 @@
+package giadatonni.GENERA._BE.exceptions;
+
+import java.util.UUID;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(UUID id) {
+        super("The resource with id" + id + " was not found");
+    }
+
+    public NotFoundException(String id) {
+        super("The resource with id" + id + " was not found");
+    }
+}
