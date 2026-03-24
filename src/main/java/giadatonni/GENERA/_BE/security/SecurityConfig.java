d6215557e69b0b7219067a7cd142c3b423d68f49
Giada <tonni.giada.210802@gmail.com>
2026-03-24 18:18:01 +0100

    adds vercel link on authorized link

--- src/main/java/giadatonni/GENERA/_BE/security/SecurityConfig.java
+++ src/main/java/giadatonni/GENERA/_BE/security/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://genera-fe.vercel.app"));
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
 
